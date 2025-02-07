c490799badf0c0f3355edf8b51b2dcf41844a94e
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-07 22:03:56 +0000

    Added helpful git commands

--- 1_intro/1_terminal_commands.js
+++ 1_intro/1_terminal_commands.js
@@ -11,4 +11,20 @@ mkdir - followed by name will create a folder
 code - followed by file name to open in editor
 node - followed by file name to execute js 
 
+GIT COMMANDS
+
+-- for new repos --
+
+git init - initialise git
+git remote add origin https://github.com/(user name here)/(insert existing repo name here) - do not create a README.md while making a repo on GitHub
+git add . - add changes
+git commit -m 'Insert comment here' - add some annotations
+git push origin main - push all changes back to the repo
+git status - check if connected to git
+
+-- for existing repos --
+
+git clone (repo link here) - download an existing repository
+git pull origin main - pull changes to your repo made by contributors
+
 */
