a7b073b995323bcf4723e81e9935149319c05028
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-07 22:20:02 +0000

    Added variables lecture notes

--- 2_foundational/4_variables.js
+++ 2_foundational/4_variables.js
@@ -4,10 +4,19 @@ Variables
 
 */
 
+let colour = 'green';
+console.log(`Value of colour: ${colour}`);
 
+let number = 42;
+console.log(`Value of number: ${number + 3}`);
 
 /* 
 
 Note:
+
+Variables are declared with the keyword: var (not much used, polutes global scope), let, and const for values that won't or shouldn't be changed
+
+= - means assignment
+`${insert variable or function call directly into this literal}` - template literal
 
 */
