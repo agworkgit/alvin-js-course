c00ed3feeb1e8fd94295b3209697f1e99475a928
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-07 22:32:40 +0000

    Practiced throwing ReferenceError

--- 2_foundational/4_variables.js
+++ 2_foundational/4_variables.js
@@ -15,6 +15,16 @@ console.log('Initial value: ' + counter);
 counter++;
 console.log('Incremented value: ' + counter);
 
+// Variables can be declared but unassigned until later e.g. below
+
+let i;
+console.log(i); // this will come up as "undefined"
+i = 100; // we now assign a value to the already declared variable
+console.log(i); // logs 100
+
+// Throwing a ReferenceError - this will appear when we reference undefined variables
+/* console.log(notdefinedyet); */
+
 /* 
 
 Note:
