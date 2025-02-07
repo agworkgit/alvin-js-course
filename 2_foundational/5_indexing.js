ee0898792144d87c3098c2190eca4c8afc0bfbe6
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-07 23:58:18 +0000

    Starting indexing lecture

---
+++ 2_foundational/5_indexing.js
@@ -0,0 +1,21 @@
+/* 
+
+Indexing
+
+*/
+
+console.log('marker'[0]); // prints only m which is the 0th index of the string
+
+let str = 'potato';
+console.log(str[5]); // prints o
+console.log(str[6]); // index past the available indices in str
+
+
+
+/* 
+
+Note:
+
+
+
+*/
