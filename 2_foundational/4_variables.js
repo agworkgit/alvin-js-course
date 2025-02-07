2c0411a517570b17134472a803c5ee613f5be884
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-07 22:24:55 +0000

    Updated notes on variables lecture

--- 2_foundational/4_variables.js
+++ 2_foundational/4_variables.js
@@ -10,13 +10,25 @@ console.log(`Value of colour: ${colour}`);
 let number = 42;
 console.log(`Value of number: ${number + 3}`);
 
+let counter = 0;
+console.log('Initial value: ' + counter);
+counter++;
+console.log('Incremented value: ' + counter);
+
 /* 
 
 Note:
 
 Variables are declared with the keyword: var (not much used, polutes global scope), let, and const for values that won't or shouldn't be changed
 
-= - means assignment
-`${insert variable or function call directly into this literal}` - template literal
+= | means assignment
+`${insert variable or function call directly into this literal}` | template literal
+
++= | addition assignment shorthand
+-= | subtraction assignment shorthand
+/= | division assignment shorthand
+*= | multiplication assignment shorthand
+++ | increment shorthand (counter + 1);
+-- | decrement
 
 */
