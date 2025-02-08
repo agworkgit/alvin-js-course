fe76de6442259cf8b62591fec5de9a4eab72a2d5
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-08 14:55:33 +0000

    Starting functions lecture

--- 2_foundational/6_conditionals.js
+++ 2_foundational/6_conditionals.js
@@ -1,6 +1,8 @@
-let number = 20;
+// A conditional is a construct that allows programmers to execute different code dependent on the value of a boolean expression.
 
 /* 
+
+Notes:
 
 Basic structure of a conditional
 
@@ -29,15 +31,54 @@ console.log((age >= 18) ? 'You are an adult!' : 'You are a kid.');
 
 */
 
+// EXAMPLE 1
+
+let number = 20;
+
+// add a new line for readability
+console.log('\n');
+
+// prints first
 console.log("before");
 
+// condition is checked top down and result printed
 if (number > 0) {
-  console.log("condition 1: the number is greater than 0");
-} else if (number < 0) {
-  console.log("condition 2: the number is less than 0");
+    console.log("condition 1: the number is positive");
+  } else {
+    console.log("condition 2: the number is negative");
+  }
+
+if (number % 2 === 0) {
+  console.log("condition 3: the number is even");
 } else {
-  console.log("catch all: the number is 0");
+  console.log("condition 4: the number is odd");
 }
 
+// prints last
 console.log("after");
 
+// add a new line for readability
+console.log('\n');
+
+// EXAMPLE 2
+
+let word = 'javascript';
+
+if (word.length > 3) {
+    console.log('long name');
+} else {
+    console.log('short name');
+    
+}
+
+// add a new line for readability
+console.log('\n');
+
+if (word[0] === 'a') {
+    console.log('word STARTS with a');
+} else {
+    console.log('word does NOT START with a');
+}
+
+// add a new line for readability
+console.log('\n');
---
+++ 2_foundational/7_functions.js
@@ -0,0 +1,6 @@
+/* 
+
+Functions are constructs that allow programmers to group multiple statements together into a single logical unit. Functions may take arguments as input and return some value as output. 
+
+*/
+
