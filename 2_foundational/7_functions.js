8ad8f72cf3fc3dd2b764ace4ed65f10dc42190a0
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-08 15:29:04 +0000

    Started functions lecture

--- 2_foundational/7_functions.js
+++ 2_foundational/7_functions.js
@@ -4,3 +4,42 @@ Functions are constructs that allow programmers to group multiple statements tog
 
 */
 
+/* function getAverage(x, y)
+{
+    return ((x + y) / 2);
+}
+
+console.log(getAverage(40, 120)); */
+
+// EXERCISE 1
+
+// Function definition
+let functionTest = function() {
+    console.log('i am a function');
+    console.log('ending function');
+};
+
+// Function call
+console.log('one'); // starts here
+functionTest(); // jumps into the function to run the code there
+console.log('two'); // comes back here
+
+// EXERCISE 2
+
+let calculateAverage = function (num1, num2) // these get replaced by actual values below in the function call
+{
+    let sum = num1 + num2;
+    let average = sum / 2;
+    console.log(average);
+}
+
+// Check operation order
+
+console.log('\n'); // newline
+console.log('a'); // first
+calculateAverage(31, 70); // we pass in values for num1, num2 // avg = 50.5
+console.log('b'); // third
+calculateAverage(12, 44); // avg = 28
+console.log('c'); // last
+
+
