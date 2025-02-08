a36ddafa45642a5c6f30e823624b35b0fb6792ca
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-08 21:48:31 +0000

    added changes to loops lecture

--- 2_foundational/8_loops.js
+++ 2_foundational/8_loops.js
@@ -4,9 +4,24 @@ Loops are a construct that programmers use control the repeated execution of cod
 
 */
 
-console.log('Starting...');
-for (let i = 0; i <= 5; i++)
+console.log('start incrementing...');
+for (let count = 0; count <= 3; count++)
 {
-    console.log(`Loop ${i}`);
+    console.log(`iteration ${count}`);
 }
+console.log('end');
+
+/* 
+
+Notes: 
+Iteration just means one cycle of the loop.
+
+*/
+
+console.log('start decrementing...');
+for (let i = 3; i >= 0; i--)
+{
+    console.log(`decrementing ${i}`);
+}
+console.log('end');
 
