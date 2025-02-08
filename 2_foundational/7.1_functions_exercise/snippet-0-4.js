2b902cc650bb8f9cb6c960fa0d84f77f4a166436
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-08 16:01:44 +0000

    Solved functions exercise snippets

---
+++ 2_foundational/7.1_functions_exercise/snippet-0-4.js
@@ -0,0 +1,16 @@
+// snippet 0-4
+
+let exclaim = function (str)
+{
+    let capitalised = str.toUpperCase();
+    return capitalised + '!!';
+}
+
+let result = exclaim('potato');
+console.log(result); // POTATO!!
+console.log(result.length); // 8
+console.log(result[0]); // P
+console.log(result[result.length -1]); // !
+
+
+
