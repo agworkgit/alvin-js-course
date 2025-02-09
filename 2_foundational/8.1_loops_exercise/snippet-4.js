fdf71059d8a66d28705b1c61e81a1f083481d9bc
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-09 17:26:04 +0000

    solved snippet-4.js loops

---
+++ 2_foundational/8.1_loops_exercise/snippet-4.js
@@ -0,0 +1,26 @@
+// snippet 4
+let word = "street";
+for (let i = 0; i < word.length; i++) {
+  console.log(i);
+  console.log(word[i]);
+}
+
+/* 
+PREDICTION - 100%
+
+What is the word length? 6
+Meaning the count will be 0, 1, 2, 3, 4, 5 as 6 is not less than 6
+
+0
+s
+1
+t
+2
+r
+3
+e
+4
+e
+5
+t
+*/
