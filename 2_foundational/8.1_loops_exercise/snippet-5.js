ecdc043489d2bb8eea77e18961fbec878aac94e4
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-09 17:29:38 +0000

    solved snippet-5.js loops

---
+++ 2_foundational/8.1_loops_exercise/snippet-5.js
@@ -0,0 +1,21 @@
+// snippet 5
+let total = 0;
+for (let i = 1; i < 5; i++) {
+  total += i;
+  console.log(total);
+}
+
+console.log("grand total: " + total);
+
+/* 
+PREDICTION - 100%
+
+1, 2, 3, 4
+
+1
+3
+6
+10
+
+grand total: 10
+*/
