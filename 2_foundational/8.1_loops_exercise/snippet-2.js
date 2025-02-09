1af7b8a4aa68cc03eeebd4fd65ed72a2c06cb337
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-09 17:16:24 +0000

    solved snippet-2.js loops

---
+++ 2_foundational/8.1_loops_exercise/snippet-2.js
@@ -0,0 +1,26 @@
+// snippet 2
+console.log("hi");
+
+for (let i = 3; i <= 7; i++) {
+  console.log("program");
+  console.log(i);
+}
+
+console.log("bye");
+
+/* 
+PREDICTION - 100%
+
+'hi'
+'program'
+'3'
+'program'
+'4'
+'program'
+'5'
+'program'
+'6'
+'program'
+'7'
+'bye'
+*/
