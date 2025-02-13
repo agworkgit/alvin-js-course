a14d944ac1080c7ec36da9700477a014da8bce27
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-13 22:48:39 +0100

    solved reverse-array.js

---
+++ 2_foundational/9.2_arrays_exercise/reverse-array.js
@@ -0,0 +1,17 @@
+// Write a function `reverseArray` that accepts an array as an argument. The function should return a
+// array containing the elements of the original array in reverse order.
+
+let reverseArray = function (array)
+{
+    let reversed = [];
+
+    for (let i = array.length - 1; i >= 0 ; i--)
+    {
+        reversed.push(array[i]);
+    }
+
+    return reversed;
+}
+
+console.log(reverseArray(["zero", "one", "two", "three"])); // ['three', 'two', 'one', 'zero']
+console.log(reverseArray([7, 1, 8])); // [8, 1, 7]
