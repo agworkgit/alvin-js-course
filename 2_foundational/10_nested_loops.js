c867ca1b22cc8c104611a87ffab55b57115fde28
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-17 16:06:09 +0000

    started nested loops lecture

---
+++ 2_foundational/10_nested_loops.js
@@ -0,0 +1,31 @@
+// Nested Loops are when we write a loop within the body of another loop. The pattern can be useful to generate "pairs" of things.
+
+for (let i = 1; i <= 5; i++) // each iteration starts the loop inside which prints 1, 2, 3
+{
+    console.log(i);
+    console.log('beginning j');
+    for (let j = 1; j <= 3; j++) // counts from 1 to 3
+    {
+        console.log('   ' + j);
+    }
+    console.log('finishing j');
+}
+
+// The complete nested loop will go 1 - 1, 2, 3 then 2 - 1, 2, 3... and so on
+
+// When we nest a loop inside another loop, the inside loop will run the number of times of the outside loop
+
+console.log('\n'); // New line
+
+// What the nested loop below is doing is printing all the possible pairs in the array
+// However, be mindful of duplicate pairs
+
+let array = ['red','blue','green','orange'];
+
+for (let i = 0; i < array.length; i++)
+{
+    for (let j = 0; j < array.length; j++)
+    {
+        console.log(array[i], array[j]);
+    }
+}
