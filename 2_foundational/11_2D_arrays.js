94ca46896de854e0dc6fc3910a6c07aee68de108
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-18 12:49:53 +0000

    starting two dimensional array lecture

---
+++ 2_foundational/11_2D_arrays.js
@@ -0,0 +1,4 @@
+// A two-dimensional array is an array that contains other arrays as elements. 
+// Programmers use multidimensional arrays when dealing with data that has many layers. 
+// It is also useful to represent a grid space, or matrix.
+
