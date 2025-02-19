5e32e796c8ffd86bf120e0d65656661c871d307e
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-19 18:11:00 +0000

    indentation

--- 2_foundational/11.1_2D_arrays_exercise/two-sum-pairs.js
+++ 2_foundational/11.1_2D_arrays_exercise/two-sum-pairs.js
@@ -2,26 +2,20 @@
 // as arguments. The function should return a 2D array containing all unique pairs of elements that
 // sum to the target.
 
-let twoSumPairs = function (numbers, target)
-{
-    let matches = [];
-    for (let i = 0; i < numbers.length; i++)
-    {
+let twoSumPairs = function (numbers, target) {
+  let matches = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      let sum = [];
 
-        for (let j = i + 1; j < numbers.length; j++)
-        {
-            let sum = [];
-
-            if (numbers[i] + numbers[j] === target)
-            {
-                sum.push(numbers[i], numbers[j]);
-                matches.push(sum);
-            }
-            
-        }
+      if (numbers[i] + numbers[j] === target) {
+        sum.push(numbers[i], numbers[j]);
+        matches.push(sum);
+      }
     }
-    return matches;
-}
+  }
+  return matches;
+};
 
 console.log(twoSumPairs([2, 3, 4, 6, 5], 8)); // [ [2, 6], [3, 5] ]
 console.log(twoSumPairs([10, 7, 4, 5, 2], 12)); // [ [10, 2], [7, 5] ]
