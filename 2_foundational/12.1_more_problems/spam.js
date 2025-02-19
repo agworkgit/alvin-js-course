05cba52c81c867d35512e4d1fcac8704832b714d
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-19 19:06:23 +0000

    solved spam.js

---
+++ 2_foundational/12.1_more_problems/spam.js
@@ -0,0 +1,35 @@
+// Write a function `spam` that accepts a 2D array as an argument. The array contains pairs as elements.
+// The first element of every pair is a number and the second element is a word. The function should
+// return a string containing the words repeated the specified number of times. See the examples.
+
+let spam = function (array)
+{
+    let returnStr = [];
+
+    for (let i = 0; i < array.length; i++)
+    {
+        let subarray = array[i];
+        let numbers = subarray[1];
+        let words = subarray[0]
+
+        for (let j = 0; j < numbers; j++)
+        {
+            returnStr.push(words);
+        }
+    }
+
+    return returnStr.join(' ');
+}
+
+let array1 = [
+    ["hi", 3],
+    ["bye", 2],
+  ];
+  console.log(spam(array1)); // 'hi hi hi bye bye'
+  
+  let array2 = [
+    ["cat", 1],
+    ["dog", 2],
+    ["bird", 4],
+  ];
+  console.log(spam(array2)); // 'cat dog dog bird bird bird bird'
