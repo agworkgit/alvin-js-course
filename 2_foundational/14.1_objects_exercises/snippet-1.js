fb193fd0c8a701cbff40048ca239c281cdadaf5d
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-22 15:25:37 +0000

    solved snippet-1.js objects

---
+++ 2_foundational/14.1_objects_exercises/snippet-1.js
@@ -0,0 +1,29 @@
+// snippet 1
+let movie = {
+    title: "Fight Club",
+    year: 1999,
+    genre: ["drama", "thriller"],
+    starring: ["Brad Pitt", "Edward Norton"],
+  };
+  
+  console.log(movie.year);
+  console.log(movie["title"]);
+  console.log(movie.genre);
+  console.log(movie.genre[0]);
+  console.log(movie.genre[1]);
+  
+  console.log(movie.duration);
+  console.log(movie["starring"][1]);
+  console.log(movie.starring.length);
+
+// PREDICTED
+// 1999
+// Fight Club
+// ['drama', 'thriller']
+// drama
+// thriller
+// undefined
+// Edward Norton
+// 2
+
+// CORRECT - 90%
